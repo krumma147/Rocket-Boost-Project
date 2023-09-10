79988775c528dfb639961d055ce5cffe2da56c96
Pomelo <64297316+krumma147@users.noreply.github.com>
2023-09-10 18:19:26 +0700

    Update movement for rockets, add obstacle, fix movement bug
    Rocket no longer bug while impact with other stuff.

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,18 +1,51 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    private Rigidbody rb; 
+    [SerializeField] private float mainThrust = 100f;
+    [SerializeField] private float rotateSpeed = 30f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = this.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ProcessThrust();
+        ProcessRotate();
+    }
+
+	private void ProcessRotate()
+	{
+        if (Input.GetKey(KeyCode.A))
+		{
+			ApplyRotation(rotateSpeed);
+		}
+		else if (Input.GetKey(KeyCode.D))
+        {
+            ApplyRotation(-rotateSpeed);
+        }
+    }
+
+	private void ApplyRotation(float rotationPerFrame)
+	{
+        rb.freezeRotation = true; //Freeze rotation from contact with other stuff in world
+		transform.Rotate(Vector3.forward * rotationPerFrame * Time.deltaTime);
+        rb.freezeRotation = false; //Unfreeze rotation
+    }
+
+	void ProcessThrust()
+	{
+		if (Input.GetKey(KeyCode.Space))
+		{
+            Debug.Log("Pressing space");
+            rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+		}
     }
 }
